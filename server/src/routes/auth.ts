a3c12caeee0e8a44804bc5c67dc8f4cd4f726b9c
L1mus <ali.firefox@yahoo.com>
2026-01-28 20:00:13 +0700

    adding Login and Register controller to auth Router

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import {strictLimiter} from "../middlewares/security.js";
 import {validationRequest} from "../middlewares/validateRequest.js";
 import {registerSchema} from "../validations/auth.validation.js";
+import { register, login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post('/login', strictLimiter,  );
+router.post('/login', strictLimiter,login);
 
-router.post('/register', strictLimiter, validationRequest(registerSchema),);
+router.post('/register', strictLimiter, validationRequest(registerSchema),register);
 
 export default router;
